cb007277945aaab60b102c2cca0e7bdadc70fd61
Mandeepkumarsaw <153371877+Mandeepkumarsaw@users.noreply.github.com>
2026-02-12 22:37:02 +0530

    Leetcode Problems

---
+++ C++ Programs/Lec-15,16 12-02-26/Lp-724.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalSum = 0;
+        for(int i=0; i<nums.size(); i++){
+            totalSum += nums[i];
+        }
+        
+        int leftSum = 0;
+        for(int i=0;i<nums.size(); i++) {
+            if(leftSum == totalSum-leftSum-nums[i]) return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
