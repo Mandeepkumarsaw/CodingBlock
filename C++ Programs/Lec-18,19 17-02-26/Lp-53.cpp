2de04204edc93448792e9d59bb8888836fd47f56
Mandeepkumarsaw <153371877+Mandeepkumarsaw@users.noreply.github.com>
2026-02-17 21:57:11 +0530

    Lp:53 Maximum SubArray using Kadane's Algorithm

---
+++ C++ Programs/Lec-18,19 17-02-26/Lp-53.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<vector>
+#include<set>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int numsSum = nums[0];
+        int TillMax = nums[0];
+
+        for(int i=1;i<nums.size();i++) {
+             numsSum = max(nums[i],numsSum+nums[i]);
+             TillMax =  max(TillMax,numsSum);
+        }
+        return TillMax;
+    }
+};
