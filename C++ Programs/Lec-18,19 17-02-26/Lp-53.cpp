db9da6ca83d13b078f12722bc4ccb7fac0d6cd8f
Mandeepkumarsaw <153371877+Mandeepkumarsaw@users.noreply.github.com>
2026-02-22 19:46:58 +0530

    Lp:53 Maximum subarray using Kadane's Algorithm with O(n)

--- C++ Programs/Lec-18,19 17-02-26/Lp-53.cpp
+++ C++ Programs/Lec-18,19 17-02-26/Lp-53.cpp
@@ -3,7 +3,7 @@
 #include<set>
 using namespace std;
 
-class Solution {   //Booyer Moore Voting algo
+class Solution {   //Kadane's algo
 public:
     int maxSubArray(vector<int>& nums) {
         int numsSum = nums[0];
