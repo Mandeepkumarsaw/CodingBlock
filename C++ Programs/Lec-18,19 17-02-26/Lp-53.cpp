915c9cb615f2565b831c074c7f5ec28b38612137
Mandeepkumarsaw <153371877+Mandeepkumarsaw@users.noreply.github.com>
2026-02-22 19:43:26 +0530

    Lp:169 Majority Elements with O(n)

--- C++ Programs/Lec-18,19 17-02-26/Lp-53.cpp
+++ C++ Programs/Lec-18,19 17-02-26/Lp-53.cpp
@@ -3,7 +3,7 @@
 #include<set>
 using namespace std;
 
-class Solution {
+class Solution {   //Booyer Moore Voting algo
 public:
     int maxSubArray(vector<int>& nums) {
         int numsSum = nums[0];
